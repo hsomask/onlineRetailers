cb8a832614938f339169b95183e16ce01f96e6e3
hsoluo <hsoluo@163.com>
2021-01-25 17:53:55 +0800

    整合swagger，然后从拦截器中去除swagger的拦截

--- mall/src/main/java/com/imooc/mall/InterceptorConfig.java
+++ mall/src/main/java/com/imooc/mall/InterceptorConfig.java
@@ -1,5 +1,6 @@
 package com.imooc.mall;
 
+import com.imooc.mall.UserLoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,10 +11,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new UserLoginInterceptor())
-				.addPathPatterns("/**")
-				.excludePathPatterns("/error", "/user/login", "/user/register", "/categories", "/products", "/products/*");
-	}
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new UserLoginInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/error", "/user/login", "/user/register", "/categories", "/products", "/products/*", "/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**",
+                        "/api", "/api-docs", "/api-docs/**");
+        //写好的拦截器需要通过这里添加注册才能生效 updated by hsoluo 20210125
+        WebMvcConfigurer.super.addInterceptors(registry);
+    }
 }
--- mall/src/main/java/com/imooc/mall/UserLoginInterceptor.java
+++ mall/src/main/java/com/imooc/mall/UserLoginInterceptor.java
@@ -4,6 +4,8 @@ import com.imooc.mall.consts.MallConst;
 import com.imooc.mall.exception.UserLoginException;
 import com.imooc.mall.pojo.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -11,31 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by 廖师兄
- * updated by hsoluo:先去掉这个拦截器，才可以配置swagger
+ * updated by hsoluo 20210125
  */
 @Slf4j
+
 public class UserLoginInterceptor implements HandlerInterceptor {
 
-	/**
-	 * true 表示继续流程，false表示中断
-	 * @param request
-	 * @param response
-	 * @param handler
-	 * @return
-	 * @throws Exception
-	 */
-	@Override
-	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		log.info("preHandle...");
-		User user = (User) request.getSession().getAttribute(MallConst.CURRENT_USER);
-		if (user == null) {
-			log.info("user=null");
-			throw new UserLoginException();
 
-//			response.getWriter().print("error");
-//			return false;
-//			return ResponseVo.error(ResponseEnum.NEED_LOGIN);
-		}
-		return true;
-	}
+    /**
+     * true 表示继续流程，false表示中断
+     *
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("preHandle...");
+        User user = (User) request.getSession().getAttribute(MallConst.CURRENT_USER);
+        String servletPath = request.getServletPath();
+        if (user == null) {
+            log.info("user=null");
+            throw new UserLoginException();
+        }
+        return true;
+    }
 }
