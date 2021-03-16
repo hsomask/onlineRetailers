bc5155577cf9dee0cdc1f4715349f38e88b600d9
hsoluo <hsoluo@163.com>
2021-03-16 17:41:01 +0800

    编写了登录的基本接口

--- backOffice/src/main/java/com/imooc/backOffice/controller/UserController.java
+++ backOffice/src/main/java/com/imooc/backOffice/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.imooc.backOffice.controller;
 
+import com.imooc.backOffice.constant.ShiroConstant;
+import com.imooc.backOffice.entity.UserEntity;
+import com.imooc.backOffice.service.UserService;
+import com.imooc.backOffice.utils.RedisUtil;
+import com.imooc.backOffice.utils.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 
 /**
  * @author:hsoluo
@@ -13,5 +21,75 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public Result<?> login(@RequestParam String username, @RequestParam String password) {
+        Result<String> result = new Result<>();
+        UserEntity user = userService.findUserByUserName(username);
+        if (user == null) {
+            result.setSuccess(false);
+            result.setMessage("无该用户，请进行注册");
+            result.setCode(400);
+        } else if (!user.getPassword().equals(password)) {
+            result.setSuccess(false);
+            result.setMessage("用户名或者密码错误");
+            result.setCode(400);
+        } else {
+            String token = userService.loginSuccess(user);
+            result.setSuccess(true);
+            result.setResult(token);
+            result.setCode(200);
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.POST)
+    public Result<?> logout() {
+        Result<?> result = new Result<>();
+        Subject sub = SecurityUtils.getSubject();
+        log.info("user: " + sub.getPrincipals());
+        UserEntity user = (UserEntity) sub.getPrincipal();
+        RedisUtil.del(ShiroConstant.LOGIN_SHIRO_CACHE + user.getId(), ShiroConstant.ROLE_SHIRO_CACHE + user.getId());
+
+        return result;
+    }
+
+
+    @RequestMapping(value = "/sign", method = RequestMethod.POST)
+    @RequiresPermissions({"1"})
+    public Result<?> sign(@RequestBody UserEntity user) {
+        Result<UserEntity> result = new Result<>();
+        if (userService.sign(user)) {
+            result.setCode(200);
+            result.setSuccess(true);
+            result.setResult(user);
+        } else {
+            result.setCode(500);
+            result.setSuccess(false);
+            result.setMessage("注册用户失败");
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/delUser", method = RequestMethod.POST)
+    public Result<?> delUser(@RequestParam Long userId){
+        Result<?> result=new Result<>();
+        if(userService.delUser(userId)){
+            result.setCode(200);
+            result.setSuccess(true);
+            result.setMessage("删除用户成功");
+        }else {
+            result.setCode(500);
+            result.setSuccess(false);
+            result.setMessage("删除用户失败");
+        }
+
+        return result;
+    }
 
 }
--- backOffice/src/main/java/com/imooc/backOffice/service/UserService.java
+++ backOffice/src/main/java/com/imooc/backOffice/service/UserService.java
@@ -7,7 +7,9 @@ import com.imooc.backOffice.mapper.UserMapper;
 import com.imooc.backOffice.utils.RedisUtil;
 import com.imooc.backOffice.utils.TokenUtil;
 import com.sun.org.apache.xml.internal.dtm.ref.sax2dtm.SAX2RTFDTM;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
@@ -15,6 +17,8 @@ import java.util.Optional;
  * @author:hsoluo
  * @date 2021/3/16 10:38
  */
+@Service
+@Slf4j
 public class UserService {
     @Autowired
     private UserMapper userMapper;
@@ -38,19 +42,19 @@ public class UserService {
     }
 
 
-    public boolean saveUser(UserEntity user) {
+    public boolean sign(UserEntity user) {
         int save = userMapper.sign(user);
         return save > 0;
     }
 
 
-    public boolean delUser(UserEntity user) {
-        Optional<UserEntity> o = Optional.ofNullable(userMapper.findUserByUserId(user.getId()));
+    public boolean delUser(Long userId) {
+        Optional<UserEntity> o = Optional.ofNullable(userMapper.findUserByUserId(userId));
         if (o.isPresent()) {
 
-            userMapper.deleteUserByUserId(user.getId());
-            roleMapper.deleteRoleByUserId(user.getId());
-            RedisUtil.del(ShiroConstant.ROLE_SHIRO_CACHE + user.getId(), ShiroConstant.LOGIN_SHIRO_CACHE + user.getId());
+            userMapper.deleteUserByUserId(userId);
+            roleMapper.deleteRoleByUserId(userId);
+            RedisUtil.del(ShiroConstant.ROLE_SHIRO_CACHE + userId, ShiroConstant.LOGIN_SHIRO_CACHE + userId);
             return true;
         } else {
             return false;
